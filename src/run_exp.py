249cf0f90dc12b22156714bda877910af1994fb6
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-11-03 16:19:15 +0900

    WTH;

--- src/run_exp.py
+++ src/run_exp.py
@@ -17,7 +17,7 @@ def main(conf_file_path):
     config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
 
     now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
-    config.dataset.name = 'Exchange'
+    config.dataset.name = 'WTH'
     sub_dir = now.strftime('%m%d_%H%M%S') + '_' + config.dataset.name
     config.seed = set_seed(config.seed)
 
